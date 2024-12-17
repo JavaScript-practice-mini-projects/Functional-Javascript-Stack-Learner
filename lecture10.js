828bd8715d09da4651e82056f42a813a7ca63406
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2024-12-17 12:13:42 +0600

    quick commit

---
+++ lecture10.js
@@ -0,0 +1,9 @@
+// var num =  [1, 2, 3, 4, 5];
+
+// num.forEach(function(item, index, arr){
+//     // console.log(item, index, arr)
+
+//     // var x = arr[index]
+
+//     console.log(index, item)
+// });
